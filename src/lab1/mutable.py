7b1e607b0b4a6c56e9f97463af573edaa42025de
Lin <1007049562@qq.com>
2020-05-25 21:39:45 +0800

    second commit

---
+++ src/lab1/mutable.py
@@ -0,0 +1,162 @@
+class Node(object):
+    def __init__(self, max_capacity=16):
+        self.capacity = max_capacity
+        self.next = None
+        self.prev = None
+        self.items = [None] * max_capacity
+        self.value = None
+        self.size = 0
+
+
+class UnrolledLinkList(object):
+    def __init__(self, root=None):
+        self.sizeAll = 0
+        self.root = root
+
+    def __iter__(self):
+        return UnrolledLinkList()
+
+    def __next__(self):
+        if self.root is None:
+            raise StopIteration
+        curNode = self.root
+        for i in range(0, curNode.size):
+            temp = curNode.elements[i]
+            return temp
+        self.root = curNode.next
+
+    # return the sizeALl about the UnrolledLinkList
+    def size(self):
+        return self.sizeAll
+
+    # Insert new element in UnrolledLinkList by index
+    def add(self, index, e):
+
+        # When the index is illegal, return directly
+        if index < 0 or index > self.sizeAll:
+            return
+        curNode = self.root
+        while index >= curNode.size:
+            if index == curNode.size:
+                break
+            index -= curNode.size
+            curNode = curNode.next
+        # If the capacity of the node to be inserted is full, to create a new node
+        if curNode.size == curNode.capacity:
+            node = Node()
+            next_node = curNode.next
+            curNode.next = node
+            node.next = next_node
+            move_idx = curNode.size // 2
+            for i in range(move_idx, curNode.size):
+                node.items[i - move_idx] = curNode.items[i]
+                curNode.items[i] = None
+                curNode.size -= 1
+                node.size += 1
+
+            if index >= move_idx:
+                index -= move_idx
+                curNode = node
+
+        for i in range(curNode.size - 1, index - 1, -1):
+            curNode.items[i + 1] = curNode.items[i]
+        curNode.items[index] = e
+        # Number of corresponding nodes plus 1
+        curNode.size += 1
+        self.sizeAll += 1
+
+    # Remove an element in UnrolledLinkList by index
+    def remove(self, index):
+        if index < 0 or index > self.sizeAll:
+            return
+        curNode = self.root
+        # Determine the specific node location of the element to delete
+        while index >= curNode.size - 1:
+            if index == curNode.size - 1:
+                break
+            index -= curNode.size
+            curNode = curNode.next
+        for i in range(index, curNode - 1, 1):
+            curNode.items[i] = curNode.items[i + 1]
+        curNode.items[curNode.size - 1] = None
+        # Number of corresponding nodes minus 1
+        curNode.size -= 1
+
+        if curNode.capacity >= curNode.size + curNode.next.size and curNode.next.capacity != -1:
+            nextNode = curNode.next
+            for i in range(0, nextNode.size):
+                curNode.items[curNode.size + 1] = nextNode.items[i]
+            curNode.size += nextNode.size
+            curNode.next = nextNode.next
+            # Number of corresponding nodes minus 1
+        self.sizeAll -= 1
+
+    # make UnrolledLinkList be a list
+    def to_list(self):
+        res = []
+        curNode = self.root
+        while curNode is not None:
+            for i in range(0, curNode.size):
+                res.append(curNode.items[i])
+            curNode = curNode.next
+        return res
+
+    # Make a list to be  UnrolledLinkList
+    def from_list(self, lst):
+        if len(lst) == 0:
+            self.root = None
+            return
+        for e in reversed(lst):
+            self.add(0, e)
+
+    # Find the specified data in the UnrolledLinkList
+    def find(self, data):
+        curNode = self.root
+        count = 0
+        while curNode is not None:
+            for i in range(0, curNode.size):
+                if data == curNode.items[i]:
+                    count += 1
+                    index = count - 1
+                    return index
+            return -1
+
+    # Utilize f to filter the UnrolledLinkList
+    def filter(self, f):
+        curNode = self.root
+        for i in range(0, curNode.size):
+            curNode.items[i] = f(curNode.items[i])
+        return self.to_list()
+
+    # Utilize the f to map the UnrolledLinkList
+    def map(self, f):
+        curNode = self.root
+        while curNode is not None:
+            for i in range(0, curNode.size):
+                curNode.items[i] = f(curNode.items[i])
+            curNode = curNode.next
+    # process structure elements to build a return value by specific functions
+    def reduce(self, f, initial_state):
+        state = initial_state
+        curNode = self.root
+        while curNode is not None:
+            for i in range(0, curNode.size):
+                state = f(state, curNode.items[i])
+            curNode = curNode.next
+        return state
+
+    # Make it empty and return None
+    def empty(self):
+        return None
+
+    # Combine two UnrolledLinkList
+    def mconcat(self, lst1, lst2):
+        temp = self.root
+        if lst1 is not None:
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = lst1.root
+        if lst2 is not None:
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = lst2.root
---
+++ src/lab1/test_mutable.py
@@ -0,0 +1,153 @@
+import unittest
+from hypothesis import given
+import hypothesis.strategies as st
+from mutable import *
+
+
+class TestMutableList(unittest.TestCase):
+    def test_size(self):
+        lst = UnrolledLinkList(Node())
+        self.assertEqual(lst.size(), 0)
+        lst.add(0, 'm')
+        self.assertEqual(lst.size(), 1)
+        lst.add(1, 'n')
+        self.assertEqual(lst.size(), 2)
+
+    def test_to_list(self):
+        self.assertEqual(UnrolledLinkList().to_list(), [])
+        lst = UnrolledLinkList(Node())
+        lst.add(0, 'm')
+        self.assertEqual(lst.to_list(), ['m'])
+        lst.add(1, 'n')
+        self.assertEqual(lst.to_list(), ['m', 'n'])
+
+
+
+    def test_from_list(self):
+        test_data = [
+            [],
+            ['m'],
+            ['m', 'n']
+        ]
+        for e in test_data:
+            lst = UnrolledLinkList(Node())
+            lst.from_list(e)
+            self.assertEqual(lst.to_list(), e)
+
+    def test_map(self):
+        lst = UnrolledLinkList(Node())
+        lst.map(str)
+        self.assertEqual(lst.to_list(), [])
+        lst = UnrolledLinkList(Node())
+        lst.from_list([1, 2, 3])
+        lst.map(str)
+        self.assertEqual(lst.to_list(), ["1", "2", "3"])
+
+    def test_reduce(self):
+        lst = UnrolledLinkList(Node())
+        self.assertEqual(lst.reduce(lambda stb, e1: stb + e1, 0), 0)
+        # sum of list
+        lst = UnrolledLinkList(Node())
+        lst.from_list([1, 2, 3])
+        self.assertEqual(lst.reduce(lambda stb, e1: stb + e1, 0), 6)
+        # size
+        test_data = [
+            [],
+            ['m'],
+            ['m', 'n']
+        ]
+        for e in test_data:
+            lst = UnrolledLinkList(Node())
+            lst.from_list(e)
+            self.assertEqual(lst.reduce(lambda stb, _: stb + 1, 0), lst.size())
+
+    @given(st.lists(st.integers()))
+    def test_from_list_to_list_equality(self, a):
+        lst = UnrolledLinkList(Node())
+        lst.from_list(a)
+        b = lst.to_list()
+        self.assertEqual(a, b)
+
+    @given(st.lists(st.integers()))
+    def test_python_len_and_list_size_equality(self, a):
+        lst = UnrolledLinkList(Node())
+        lst.from_list(a)
+        self.assertEqual(lst.size(), len(a))
+
+    @given(a=st.lists(st.integers()), b=st.lists(st.integers()), c=st.lists(st.integers()))
+    def test_monoid_associativity(self, a, b, c):
+        lst1 = UnrolledLinkList(Node())
+        lst2 = UnrolledLinkList(Node())
+        lst3 = UnrolledLinkList(Node())
+        lst1.from_list(a)
+        lst2.from_list(b)
+        lst3.from_list(c)
+        lst_test1 = UnrolledLinkList(Node())
+        lst_testb = UnrolledLinkList(Node())
+        lst_testb.mconcat(lst1, lst2)
+        lst_test1.mconcat(lst_testb, lst3)
+        lst1 = UnrolledLinkList(Node())
+        lst2 = UnrolledLinkList(Node())
+        lst3 = UnrolledLinkList(Node())
+        lst1.from_list(a)
+        lst2.from_list(b)
+        lst3.from_list(c)
+        lst_test2 = UnrolledLinkList(Node())
+        lst_testb = UnrolledLinkList(Node())
+        lst_testb.mconcat(lst2, lst3)
+        lst_test2.mconcat(lst1, lst_testb)
+        lst_1 = lst_test1.to_list()
+        lst_2 = lst_test2.to_list()
+        self.assertEqual(lst_1, lst_2)
+
+    @given(st.lists(st.integers()))
+    def test_monoid_identity(self, data):
+        lst = UnrolledLinkList(Node())
+        lst.from_list(data)
+        lst2 = UnrolledLinkList(Node())
+        lst_concat = UnrolledLinkList(Node())
+        lst_concat.mconcat(lst, lst2.empty())
+        b = lst_concat.to_list()
+        self.assertEqual(b, data)
+
+        lst_concat = UnrolledLinkList(Node())
+        lst_concat.mconcat(lst2.empty(), lst)
+        b = lst_concat.to_list()
+        self.assertEqual(b, data)
+
+    def test_iter(self):
+        x = [1, 2, 3]
+        lst = UnrolledLinkList(Node())
+        lst.from_list(x)
+        i = 0
+        tmp = []
+        for e in lst.root.items:
+            i += 1
+            if i > len(x):
+                break
+            tmp.append(e)
+        self.assertEqual(x, tmp)
+        self.assertEqual(lst.to_list(), tmp)
+        i = iter(UnrolledLinkList())
+        self.assertRaises(StopIteration, lambda: next(i))
+
+    def test_find(self):
+        x = ['m', 'n', 'o']
+        lst = UnrolledLinkList(Node())
+        lst.from_list(x)
+        index = lst.find('n')
+        self.assertEqual(0, index)
+
+    def test_filter(self):
+        def f(_ze):
+            res = _ze + 1
+            return res
+
+        _ze = [1, 2, 3]
+        lst = UnrolledLinkList(Node())
+        lst.from_list(_ze)
+        lst.filter(f)
+        self.assertEqual([2, 3, 4], lst.to_list())
+
+if __name__ == '__main__':
+    unittest.main()
